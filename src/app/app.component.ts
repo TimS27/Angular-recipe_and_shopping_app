026ae20aa2155a0224887c60416fd6b23a3d0895
Tim <timschoenwald@googlemail.com>
2022-03-16 17:33:31 +0100

    added navigation functionality used event-binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe_and_shopping_app';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+    console.log(feature);
+  }
 }
